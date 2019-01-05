bc4492566933ab7407351785b96e006b39dd5556
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-01-05 11:46:23 +0100

    vendor: Disable art debug and enable minimize debug info
    Change-Id: I12b65c532201bf31f12cfb1d62c4a8bbe2204e2f
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -40,6 +40,14 @@ PRODUCT_PACKAGES += \
 # Disable extra StrictMode features on all non-engineering builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 
+# Do not include art debug targets
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
 # Gapps
 ifeq ($(WITH_GAPPS),true)
 $(call inherit-product-if-exists, vendor/google/gms/config.mk)
