6464671c3676960dc13ced4093b0eff0904df533
Ethan Chen <intervigil@gmail.com>
2019-06-22 14:12:40 -0700

    build: Separate platform macros from QCOM platform definition
    * The macros used in the file formerly called "qcom_utils.mk" are
      actually generic and not QCOM-specific. Move the QCOM_BOARD definitions
      to a QCOM-specific location, and name the remaning macros something more
      generic.
    
    Change-Id: I6747bc9201e56b456caf251122057d7dbc233f1c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -1,2 +1,5 @@
-# Include QCOM macros
-include vendor/fuse/build/core/qcom_utils.mk
+# Include board/platform macros
+include vendor/fuse/build/core/utils.mk
+
+# Include vendor platform definitions
+include vendor/fuse/build/core/vendor/*.mk
