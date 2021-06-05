2d8848c4d4239c089e4fe1151d2ce772673bcbf4
Sam Mortimer <sam@mortimer.me.uk>
2021-06-04 23:25:41 -0700

    vendor: Disable extra StrictMode features on all non-engineering builds
    Change-Id: Ib052fba60e12497f79913c8030f59af40b6bc32f
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -37,6 +37,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     apns-conf.xml
 
+# Disable extra StrictMode features on all non-engineering builds
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
+
 # Gapps
 ifeq ($(WITH_GAPPS),true)
 $(call inherit-product-if-exists, vendor/google/gms/config.mk)
