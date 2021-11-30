san9-s-dumpster/android_vendor_ssos

vendor: Use Pixel boot animation
Signed-off-by: AshutoshSundresh <ashutoshsundresh@gmail.com>

config/common.mk
@@ -48,10 +48,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 endif
 endif
 
-# Boot Animation
-PRODUCT_COPY_FILES += \
-    vendor/ssos/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-
 # Blur properties
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
@@ -111,6 +107,9 @@ $(call inherit-product, vendor/ssos/config/fonts.mk)
 
 # Inherit from packages config
 $(call inherit-product, vendor/ssos/config/packages.mk)
+
+# Inherit from bootanimation config
+$(call inherit-product, vendor/ssos/config/bootanimation.mk)
 
 # Inherit from rro_overlays config
 $(call inherit-product, vendor/ssos/config/rro_overlays.mk)

paperboy72543/android_vendor_fuse

Signed-off-by: ItsVixano <vixanonew@gmail.com>
Change-Id: Ib9b463848c494cf641c0e52d62cf40b5e32728a3
Signed-off-by: SparXFusion <s2234nadar@gmail.com>

config/common.mk
@@ -22,6 +22,9 @@ $(call inherit-product, vendor/fuse/config/versioning.mk)
 # Inherit from our kernel/header generator
 $(call inherit-product, vendor/fuse/config/BoardConfigFuse.mk)
 
+# Inherit boot animation
+$(call inherit-product, vendor/fuse/config/bootanimation.mk)
+
 # Vendor overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/fuse/overlay
 